poco-f5-mable/android_frameworks_base_new

SystemUI: Set load up hint when expanding QuickSettings
* To avoid jank when expanding Quick Settings, we must ensure that HWUI can handle the workload to prevent upcoming spikes,
  especially under heavy load. This can be achieved by sending hints before and after requestLayout is called

test: play media to show QS media panel -> expand/collapse quick settings

Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -2040,11 +2040,22 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             mKeyguardUserSwitcherController.setAlpha(alpha);
         }
     }
+    
+    private void boostFrames() {
+        View viewRoot = (mView != null) ? mView.getRootView() : null;
+        if (viewRoot != null) {
+            viewRoot.getViewRootImpl().notifyRendererOfExpensiveFrame();
+        }
+        this.mView.requestLayout();
+        if (viewRoot != null) {
+            viewRoot.getViewRootImpl().notifyRendererOfExpensiveFrame();
+        }
+    }
 
     @Override
     public void transitionToExpandedShade(long delay) {
         mNotificationStackScrollLayoutController.goToFullShade(delay);
-        mView.requestLayout();
+        boostFrames();
         mAnimateNextPositionUpdate = true;
     }
 
@@ -2958,7 +2969,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                         }
                     });
             // Make sure a layout really happens.
-            this.mView.requestLayout();
+            boostFrames();
         }
 
         setListening(true);
@@ -4347,7 +4358,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         //A layout will ensure that onComputeInternalInsets will be called and after that we can
         // resize the layout. Make sure that the window stays small for one frame until the
         // touchableRegion is set.
-        mView.requestLayout();
+        boostFrames();
         mNotificationShadeWindowController.setForceWindowCollapsed(true);
         postToView(() -> {
             mNotificationShadeWindowController.setForceWindowCollapsed(false);

ATI-dumpster/frameworks_base-new

packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -1995,11 +1995,23 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             mKeyguardUserSwitcherController.setAlpha(alpha);
         }
     }
+    
+    private void boostFrames() {
+        if (mView != null && mView.getViewRootImpl() != null) {
+            mView.getViewRootImpl().notifyRendererOfExpensiveFrame();
+        }
+    }
+    
+    private void boostFramesDuringRelayout() {
+        boostFrames();
+        this.mView.requestLayout();
+        boostFrames();
+    }
 
     @Override
     public void transitionToExpandedShade(long delay) {
         mNotificationStackScrollLayoutController.goToFullShade(delay);
-        mView.requestLayout();
+        boostFramesDuringRelayout();
         mAnimateNextPositionUpdate = true;
     }
 
@@ -2913,7 +2925,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                         }
                     });
             // Make sure a layout really happens.
-            this.mView.requestLayout();
+            boostFramesDuringRelayout();
         }
 
         setListening(true);
@@ -4316,7 +4328,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         //A layout will ensure that onComputeInternalInsets will be called and after that we can
         // resize the layout. Make sure that the window stays small for one frame until the
         // touchableRegion is set.
-        mView.requestLayout();
+        boostFramesDuringRelayout();
         mNotificationShadeWindowController.setForceWindowCollapsed(true);
         postToView(() -> {
             mNotificationShadeWindowController.setForceWindowCollapsed(false);